070bf505e0abc3fbb52c15bd995ca5394fda4461
root <stanlym315@gmail.com>
2023-02-21 08:39:44 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print_alphabets
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char x;
+	for(x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
