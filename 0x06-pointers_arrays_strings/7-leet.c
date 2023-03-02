fba10e3164321b38e8385ed086ffe243b82ebcbe
root <stanlym315@gmail.com>
2023-03-02 06:55:56 -0800

    ./100-rot13

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 char *leet(char *str)
 {
 	int index1 = 0, index2;
-	char leet[8] = {'o', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[index1])
 	{
