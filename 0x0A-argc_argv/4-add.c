bf350f82787e974edadfccd8adbf500598ff850f
mosetf <stanlym315@gmail.com>
2023-03-10 11:21:10 -0800

    t for hard

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,12 +2,13 @@
 	#include <stdlib.h>
 	#include <ctype.h>
 	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
+
+/**
+ * check_num - check string there are digit
+ * @str: array str
+ * Return: Always 0 (Success)
+ */
+
 	int check_num(char *str)
 	{
 		/*Declaring variables*/
@@ -31,25 +32,22 @@
 	}
 
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
 
 
 	int main(int argc, char *argv[])
-
-
 	{
 
 
 		/*Declaring variables*/
 		int count;
 		int str_to_int;
-    int sum = 0;
+int sum = 0;
 
 
 		count = 1;
