2fbff4ed48fca04f531e77d86c570b47cc87c152
mosetf <stanlym315@gmail.com>
2023-03-14 04:06:14 -0700

    betty is full of suprises;)

--- 0-create_array.c/0-create_array.c
+++ 0-create_array.c/0-create_array.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: creat array of size size and assign char c
- * Return: pointer to array, NULL if fail
- */
+* create_array - create array of size size and assign char c
+* @size: size of array
+* @c: char to assign
+* Description: creat array of size size and assign char c
+* Return: pointer to array, NULL if fail
+*/
 
 char *create_array(unsigned int size, char c)
 {
-	char *str;
+char *str;
 
-	unsigned int i;
+unsigned int i;
 
-	str = malloc(sizeof(char) * size);
+str = malloc(sizeof(char) * size);
 
-	if (size == 0 || str == NULL)
+if (size == 0 || str == NULL)
 
-		return (NULL);
+return (NULL);
 
-	for (i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 
-		str[i] = c;
+str[i] = c;
 
-	return (str);
+return (str);
 
 }
