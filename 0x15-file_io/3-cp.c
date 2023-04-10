6c7f4488607840484852e55a30067109d9163cbd
mosetf <stanlym315@gmail.com>
2023-04-10 12:37:52 -0700

    update 3-cp file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,21 +8,21 @@
 
 int main(int argc, char **argv)
 {
-	int fp, fp2, filecheck;
+	int fd, fd2, filecheck;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fp = open(argv[1], O_RDONLY);
-	if (fp == -1)
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fp2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fp2 == -1)
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((filecheck = read(fp, buffer, 1024)) > 0)
+	while ((filecheck = read(fd, buffer, 1024)) > 0)
 	{
 		if (filecheck == -1)
 		{
@@ -41,10 +41,10 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fp) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fp %d\n", fp), exit(100);
-	if (close(fp2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fp %d\n", fp2), exit(100);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 
 	return (0);
 }
