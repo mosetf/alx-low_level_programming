c398d02cc556ed585849d3010e2c5654e12e1385
mosetf <stanlym315@gmail.com>
2023-05-08 15:49:44 +0000

    program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -259,7 +259,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		end_elf(o);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
@@ -274,14 +274,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
-	print_magic(header->e_ident);
-	print_class(header->e_ident);
-	print_data(header->e_ident);
-	print_version(header->e_ident);
-	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
-	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
+	printf_magic(header->e_ident);
+	printf_class(header->e_ident);
+	printf_data(header->e_ident);
+	printf_version(header->e_ident);
+	printf_osabi(header->e_ident);
+	printf_abi(header->e_ident);
+	printf_type(header->e_type, header->e_ident);
+	printf_input(header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(o);
