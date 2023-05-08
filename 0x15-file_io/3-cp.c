aa27448a4547f1a394c124c897634f67a36a1177
mosetf <stanlym315@gmail.com>
2023-05-08 14:35:56 +0000

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,40 +7,58 @@
  *
  * Return: 0 on success, exit with code 97, 98, 99, or 100 on failure
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int file_from, file_to, r, w, c;
-	char buffer[1024];
+	int fd_from, fd_to;
+	ssize_t nread, nwritten;
+	char buf[BUFSIZE];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from < 0)
-		dprintf(STDERR_FILENO, "Error:Can't read from file%s\n", argv[1]), exit(98);
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to < 0)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-
-	while ((r = read(file_from, buffer, 1024)) > 0)
 	{
-		w = write(file_to, buffer, r);
-		if (w < 0)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (EXIT_FAILURE);
 	}
 
-	if (r < 0)
-		dprintf(STDERR_FILENO, "Error:Can't read from file%s\n", argv[1]), exit(98);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
 
-	c = close(file_from);
-	if (c < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		return (EXIT_FAILURE);
+	}
 
-	c = close(file_to);
-	if (c < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	while ((nread = read(fd_from, buf, BUFSIZE)) > 0)
+	{
+		nwritten = write(fd_to, buf, nread);
+		if (nwritten == -1 || nwritten != nread)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			return (EXIT_FAILURE);
+		}
+	}
 
-	return (0);
+	if (nread == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		return (EXIT_FAILURE);
+	}
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
+		return (EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define BUFSIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
